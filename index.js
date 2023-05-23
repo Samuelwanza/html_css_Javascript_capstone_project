489575209a6f62625754244a89cf0cd16710ef7d
Samuel wanza <s.wanza@alustudent.com>
2023-05-23 02:43:17 +0200

    styled the program section

---
+++ index.js
@@ -0,0 +1,38 @@
+const featuredSpeakers = [
+  {
+    speakerName: "Yonchai Benkler",
+    image_url: "images/speaker_01 1.png",
+    role: "Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School",
+    bio: "Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006",
+  },
+  {
+    speakerName: "SohYeong Noh",
+    image_url: "images/speaker2.png",
+    role: "Director of Art Centre Nabi and a board member of CC Korea",
+    bio: "As the main venue for new media art production in Korea, Nabi promotes cross-disciplinary collaboration and understanding among science technology, humanities, and the arts.",
+  },
+  {
+    speakerName: "Lila tretikov",
+    image_url: "images/speaker3",
+    role: "Executive Director of the Wikimedia Foundation",
+    bio: "Lila Tretikov is the Executive of the Wikimedia Foundation, the nonprofit organization that operates Wikipedia. Wikipedia is freely available in 290 languag-es and used by nearly half a billion people around the world every month.",
+  },
+  {
+    speakerName: "Kilnam Chon",
+    image_url: "images/speaker4.png",
+    role: " ",
+    bio: "Kilnam Chon helped bring the internet to Asia and is an outspoken advocate for the open web and digital com-mons. In 2012. he was inducted into the inaugural class of the Internet Society’s (ISOC) Internet Hall of Fame",
+  },
+  {
+    speakerName: "Julia Leda",
+    image_url: "images/speaker5",
+    role: "President of Young Pirates of Europe",
+    bio: "European ingetration, political democracy and participation of youth through online as her major condern, Reda’s report outlining potential changes to EU copyright law was approved by the Parliament in July",
+  },
+  {
+    speakerName: "Ryan Merkley",
+    image_url: "images/speaker6",
+    role: "CEO of Creativve Commons, ex COO of the Mozilla Foundation",
+    bio: "Ryan had been leading open-source projects at the Mozilla Foundation such as the open-source move-ment",
+  },
+];
